2c82b59a1ffdb6b767c86c93685dcfe307f68779
fonkamloic <fonkamloic@gmail.com>
2019-11-20 21:51:22 +0100

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:good/product_manager.dart';
+import 'package:good/screens/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Center(
+          child: ListTile(
+            onTap: () {},
+            title: Text('Menu'),
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: Text("EasyList"),
       ),
-      body: ProductManager(),
+      body: AuthScreen(),
     );
   }
 }
