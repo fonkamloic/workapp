92001e6aa797397da5daccf0042e8206393fb9b5
fonkamloic <fonkamloic@gmail.com>
2019-11-21 21:53:13 +0100

    udpates

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,11 +1,37 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProductScreen extends StatelessWidget {
+  final String title;
+  final String image;
+  ProductScreen(this.title, this.image);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(child: Text("Product details")),
+    return WillPopScope(
+      onWillPop: () {
+        print("Back botton pressed");
+        Navigator.pop(context, true);
+        return Future.value(true);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Product Detail"),
+        ),
+        body: Container(
+          child: Center(
+              child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image.asset(image),
+              Text(title),
+              RaisedButton(
+                color: Colors.redAccent,
+                child: Text("Delete"),
+                onPressed: () => Navigator.pop(context, true),
+              ),
+            ],
+          )),
+        ),
       ),
     );
   }
